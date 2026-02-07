c1912aa34fcee3293044a5e4f51073628ed04118
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-07 22:18:53 +0530

    Added headers to fetch request

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ async function generateJoke(){
     const joke = createMessageElement();
     setElementContent(joke,'<i class="fa-solid fa-ellipsis"></i>');
     appendMessage(joke);
+
+
+        headers:{
+            Accept:"application/json",
+        },
+    
